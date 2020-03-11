3ca2abb37a18580f25fd46fc22a028a44df009a6
Nick Korintus <nicholas.korintus@gmail.com>
2020-03-11 11:56:09 -0400

    Updates

---
+++ precache-manifest.396a7959f44f30e00daae9db4679fa91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ebc8f05e30afb743d1618ec73e4169",
+    "url": "/react-light-switch/index.html"
+  },
+  {
+    "revision": "dba2512ee009a1b1c150",
+    "url": "/react-light-switch/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4ea9b0bedea74af4669e",
+    "url": "/react-light-switch/static/js/2.409d825a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-light-switch/static/js/2.409d825a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dba2512ee009a1b1c150",
+    "url": "/react-light-switch/static/js/main.ae4a0b8a.chunk.js"
+  },
+  {
+    "revision": "35453ee024601d059880",
+    "url": "/react-light-switch/static/js/runtime-main.e61d32ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-light-switch/static/media/logo.5d5d9eef.svg"
+  }
+]);
